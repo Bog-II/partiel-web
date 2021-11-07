1281750eb70010c60df15f001858155ac95574b4
Bogdan <bogsacha@gmail.com>
2021-11-07 17:05:31 +0100

    routes creation

--- src/routes/api.route.ts
+++ src/routes/api.route.ts
@@ -7,11 +7,15 @@ import { employeesRouter } from './employees.route';
 import { officesRouter } from './offices.route';
 import { ordersRouter } from './orders.route';
 import { paymentsRouter } from './payments.route';
+import { productlinesRouter } from './productlines.route';
+import { productsRouter } from './products.route';
 
 apiRouter.use('/customers', customersRouter);
 apiRouter.use('/employees', employeesRouter);
 apiRouter.use('/offices', officesRouter);
 apiRouter.use('/orders', ordersRouter);
 apiRouter.use('/payments', paymentsRouter);
+apiRouter.use('/products', productsRouter);
+apiRouter.use('/productlines', productlinesRouter);
 
 export { apiRouter };
--- src/routes/customers.route.ts
+++ src/routes/customers.route.ts
@@ -3,4 +3,13 @@ import express from 'express';
 const router = express.Router();
 
 
+router.get('/', );
+router.get('/:customer_id', );
+
+router.post('/', );
+
+router.put('/:customer_id', );
+
+router.delete('/:customer_id', );
+
 export { router as customersRouter };

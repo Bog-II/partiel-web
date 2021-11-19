a0281fb606d2d9ad3d97d2eab6ad03b8ab72d4b7
Bogdan <bogsacha@gmail.com>
2021-11-19 01:16:18 +0100

    users sql model

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -1,1 +1,38 @@
 import { dbPool } from '../config/mysql.config';
+
+export const getCustomerByEmail = (
+  email: string,
+  callback: (err: Error | null, res: Object) => void
+) => {
+  dbPool.query(
+    'SELECT * FROM users WHERE emailId = ?',
+    email,
+    (err, resQuery) => {
+      if (err) {
+        callback(err, {});
+      } else {
+        callback(null, resQuery);
+      }
+    }
+  );
+};
+
+export const createNewUser = (
+  email: string,
+  userPassword: string,
+  firstname: string,
+  lastname: string,
+  callback: (err: Error | null) => void
+) => {
+  dbPool.query(
+    'INSERT INTO `users`(`emailId`,`password`,`firstName`,`lastName`) values (?,?,?,?)',
+    [email, userPassword, firstname, lastname],
+    (err, _) => {
+      if (err) {
+        callback(err);
+      } else {
+        callback(null);
+      }
+    }
+  );
+};
